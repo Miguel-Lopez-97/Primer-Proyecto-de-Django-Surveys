000fcc1af9c7fec29671131a0979a275a825b638
Miguel-Lopez-97 <miguel.m.lopez.p@gmail.com>
2022-03-11 22:53:53 -0500

    fixed

--- systempoll/settings.py
+++ systempoll/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kfu!b3k$v@!rp)=o_y0x9atv$$pc5^j)xs=_vp^k6y(ss_bp2x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://djangopollseducamas.herokuapp.com/',
 'localhost',
@@ -108,7 +108,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+#STATIC_URL = 'static/'
 
 STATIC_URL = os.path.join(BASE_DIR,'static')
 # Activate Django-Heroku.
